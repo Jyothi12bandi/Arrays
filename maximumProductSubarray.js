c758137c90c576df56f668e5268d962b81f3ccdf
JyothiBandi <AJsaathma@gmail.com>
2025-05-02 13:41:11 +0530

    Finding the maximum subarray product

---
+++ maximumProductSubarray.js
@@ -0,0 +1,24 @@
+let arr=[-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+let n = arr.length;
+let maxProduct = Number.MIN_SAFE_INTEGER;
+let start=0,end=0;
+let right = 1;
+let left = 1;
+for(let i=0;i<n;i++)
+{
+    if(left ===0)
+    {
+        left = 1;
+    }
+    if(right === 0)
+    {
+        right = 1;
+    }
+
+    left *= arr[i];
+    let j = n-i-1;
+    right *= arr[j];
+    maxProduct = Math.max(left,right,maxProduct);
+}
+console.log(maxProduct);
